4c59a87281c3c8b24c38503cd930e3bf36f14905
Mhedii <mehedi1234.mh@gmail.com>
2021-10-18 22:18:59 +0600

    first

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDqH9yiDYjGIrvZpUXGPXcPiVWpTQuc1E",
+    authDomain: "astha-firebase-authentication.firebaseapp.com",
+    projectId: "astha-firebase-authentication",
+    storageBucket: "astha-firebase-authentication.appspot.com",
+    messagingSenderId: "642205716914",
+    appId: "1:642205716914:web:b6d01a02b4405e7da0d4ab"
+};
+export default firebaseConfig;
